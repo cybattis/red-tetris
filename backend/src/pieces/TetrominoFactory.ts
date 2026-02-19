4c2f844c32468d6e4fb31338937deec54b180baa
cybattis <cybattis@student.42lyon.fr>
2026-02-19 15:12:05 +0100

    feat: export Tetromino pieces and dictionary for improved accessibility

--- backend/src/pieces/TetrominoFactory.ts
+++ backend/src/pieces/TetrominoFactory.ts
@@ -1,41 +1,53 @@
-const T_PIECE: number[][] = [
+import { PieceType } from '../types/piece';
+
+export const T_PIECE: number[][] = [
   [0, 1, 0],
   [1, 1, 1],
   [0, 0, 0],
 ];
 
-const I_PIECE: number[][] = [
+export const I_PIECE: number[][] = [
   [0, 1, 0, 0],
   [0, 1, 0, 0],
   [0, 1, 0, 0],
   [0, 1, 0, 0],
 ];
 
-const J_PIECE: number[][] = [
+export const J_PIECE: number[][] = [
   [1, 0, 0],
   [1, 1, 1],
   [0, 0, 0],
 ];
 
-const L_PIECE: number[][] = [
+export const L_PIECE: number[][] = [
   [0, 0, 1],
   [1, 1, 1],
   [0, 0, 0],
 ];
 
-const O_PIECE: number[][] = [
+export const O_PIECE: number[][] = [
   [1, 1],
   [1, 1],
 ];
 
-const S_PIECE: number[][] = [
+export const S_PIECE: number[][] = [
   [0, 1, 1],
   [1, 1, 0],
   [0, 0, 0],
 ];
 
-const Z_PIECE: number[][] = [
+export const Z_PIECE: number[][] = [
   [1, 1, 0],
   [0, 1, 1],
   [0, 0, 0],
 ];
+
+export const TETROMINO_DICTIONARY: Record<PieceType, number[][]> = {
+  [PieceType.T]: T_PIECE,
+  [PieceType.I]: I_PIECE,
+  [PieceType.J]: J_PIECE,
+  [PieceType.L]: L_PIECE,
+  [PieceType.O]: O_PIECE,
+  [PieceType.S]: S_PIECE,
+  [PieceType.Z]: Z_PIECE,
+};
