e1e4ab58ccdcea35c52416a8081f20e918dbc9bf
cybattis <cybattis@student.42lyon.fr>
2026-02-20 16:09:57 +0100

    feat: enhance WebSocketManager with game handling and player input events

--- backend/src/socket/WebSocketManager.ts
+++ backend/src/socket/WebSocketManager.ts
@@ -1,5 +1,9 @@
+import { SocketEvents } from '@shared/types/game';
 import { Server as HttpServer } from 'node:http';
 import { Server, Socket } from 'socket.io';
+import { Player } from 'src/classes/Player';
+import { gameManager } from 'src/server';
+import { ToStringFormat } from 'src/utils/helpers';
 
 export class WebSocketManager {
   public io: Server;
@@ -16,12 +20,6 @@ export class WebSocketManager {
     this.io.on('connection', (socket: Socket) => {
       console.log(`Client connected: ${socket.id}`);
 
-      // Basic echo test
-      socket.on('message', (data: any) => {
-        console.log(`Received message: ${data}`);
-        socket.emit('message', `Echo: ${data}`);
-      });
-
       wsRoomHandler(socket);
       wsGameHandler(socket);
 
@@ -29,6 +27,10 @@ export class WebSocketManager {
         console.log(`Client disconnected: ${socket.id}`);
       });
     });
+  }
+
+  public close(done?: () => void): void {
+    this.io.close(() => done?.());
   }
 }
 
@@ -40,8 +42,45 @@ function wsRoomHandler(socket: Socket) {
 }
 
 function wsGameHandler(socket: Socket) {
-  socket.on('game', (data: any) => {
-    console.log(`Received game message: ${data}`);
-    socket.emit('message', `Echo: ${data}`);
+  socket.on('START_GAME', (data: SocketEvents<'START_GAME'>) => {
+    console.log(`Received game message: ${ToStringFormat(data)}`);
+
+    const seed = Date.now(); // For example, use current timestamp as seed
+    const player = new Player(socket.id);
+    const game = gameManager.createGame(player, {
+      gravity: 1,
+      gameSpeed: 1,
+      ghostPiece: true,
+      boardWidth: 10,
+      boardHeight: 20,
+      nextPieceCount: 5,
+    }, seed);
+
+    game.start();
+
+    socket.emit('GAME_STARTED', { gameId: game.id });
+  });
+
+  socket.on('STOP_GAME', (data: SocketEvents) => {
+    console.log(ToStringFormat(data));
+
+    const { gameId } = data.data as { gameId: string };
+    const game = gameManager.getGame(gameId);
+    if (game) {
+      game.stopGame();
+      socket.emit('GAME_STOPPED', { gameId });
+    } else {
+      console.warn(`Game not found: ${gameId}`);
+    }
+  });
+
+  socket.on('PLAYER_INPUT', (payload: SocketEvents<'PLAYER_INPUT'>) => {
+    console.log(ToStringFormat(payload));
+
+    const { gameId, input } = payload.data;
+    const game = gameManager.getGame(gameId)?.setPlayerInput(input);
+    if (!game) {
+      console.warn(`Game not found: ${gameId}`);
+    }
   });
 }
