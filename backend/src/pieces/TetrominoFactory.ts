9cd69a93bfbdb679ae653f1c3b1e378f76f04279
cybattis <cybattis@student.42lyon.fr>
2026-02-22 17:42:21 +0100

    refactor: update Tetromino definitions to use IPiece interface

--- backend/src/pieces/TetrominoFactory.ts
+++ backend/src/pieces/TetrominoFactory.ts
@@ -1,48 +1,76 @@
-import { PieceType } from '../types/piece';
+import { IPiece, PieceType } from '../types/IPiece';
 
-export const T_PIECE: number[][] = [
-  [0, 1, 0],
-  [1, 1, 1],
-  [0, 0, 0],
-];
+export const T_PIECE: IPiece = {
+  type: PieceType.T,
+  id: 1,
+  shape: [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+};
 
-export const I_PIECE: number[][] = [
-  [0, 0, 0, 0],
-  [1, 1, 1, 1],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-];
+export const I_PIECE: IPiece = {
+  type: PieceType.I,
+  id: 2,
+  shape: [
+    [0, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ],
+};
 
-export const J_PIECE: number[][] = [
-  [1, 0, 0],
-  [1, 1, 1],
-  [0, 0, 0],
-];
+export const J_PIECE: IPiece = {
+  type: PieceType.J,
+  id: 3,
+  shape: [
+    [1, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+};
 
-export const L_PIECE: number[][] = [
-  [0, 0, 1],
-  [1, 1, 1],
-  [0, 0, 0],
-];
+export const L_PIECE: IPiece = {
+  type: PieceType.L,
+  id: 4,
+  shape: [
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+};
 
-export const O_PIECE: number[][] = [
-  [1, 1],
-  [1, 1],
-];
+export const O_PIECE: IPiece = {
+  type: PieceType.O,
+  id: 5,
+  shape: [
+    [1, 1],
+    [1, 1],
+  ],
+};
 
-export const S_PIECE: number[][] = [
-  [0, 1, 1],
-  [1, 1, 0],
-  [0, 0, 0],
-];
+export const S_PIECE: IPiece = {
+  type: PieceType.S,
+  id: 6,
+  shape: [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0],
+  ],
+};
 
-export const Z_PIECE: number[][] = [
-  [1, 1, 0],
-  [0, 1, 1],
-  [0, 0, 0],
-];
+export const Z_PIECE: IPiece = {
+  type: PieceType.Z,
+  id: 7,
+  shape: [
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ],
+};
 
-export const TETROMINO_DICTIONARY: Record<PieceType, number[][]> = {
+export const TETROMINO_DICTIONARY: Record<PieceType, IPiece> = {
   [PieceType.T]: T_PIECE,
   [PieceType.I]: I_PIECE,
   [PieceType.J]: J_PIECE,
