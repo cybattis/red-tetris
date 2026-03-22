4c1ddf0bf36f382c881292662a3e6a8967a3ac44
cybattis <cybattis@student.42lyon.fr>
2026-03-22 13:26:54 +0100

    fix: update production configuration and server URL handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,12 @@ import { server } from './server.js';
 import { Logger } from './utils/helpers.js';
 
 const PORT = process.env.PORT || 8000;
+const VITE_SERVER_URL = process.env.VITE_SERVER_URL || `http://localhost:${PORT}`;
 
 // Only listen if this file is run directly (not imported)
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
   server.listen(PORT, () => {
-    Logger.info(`Server running on http://localhost:${PORT}`);
+    Logger.info(`Server running on ${VITE_SERVER_URL}`);
   });
 }
 
--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
 echo "Starting production server..."
 
+mkdir -p prod/public
+
 cd frontend || exit
-echo "Installing frontend dependencies and building the app..."
-npm install && npm run build
+echo "Installing frontend dependencies and building the client..."
+npm install
+npm run build
 
-echo "Starting backend server..."
+echo "Copying frontend build to production directory..."
+cp -r ../frontend/dist/* ../prod/public
+
+echo "Frontend build completed."
+
+echo "Building backend server..."
 cd ../backend || exit
-
-echo "Copy frontend build"
 
 echo "Installing backend dependencies..."
 npm install && npm run build
 
-cp -r ../frontend/dist ./dist/public
+echo "Copying backend server files to production directory..."
+cp package.json ../prod
+cp -r node_modules ../prod/node_modules
+cp -r dist/shared ../prod/shared
+cp -r dist/backend ../prod/backend
 
+rm -rf /app/backend
+rm -rf /app/shared
+rm -rf /app/frontend
+
+echo "Starting production server..."
+cd /app/prod || exit
 npm run prod
 
-echo "Production server is running. Access the app at http://localhost:3000"
+echo "Production server is running. Access it at ${VITE_SERVER_URL}"
