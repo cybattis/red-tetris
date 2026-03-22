d9ae4634e1f835d0ed224d688221fa5e72b44289
cybattis <cybattis@student.42lyon.fr>
2026-03-22 12:13:43 +0100

    refactor(backend): update project configuration and improve Docker setup for production

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -3,17 +3,18 @@ echo "Starting production server..."
 
 cd frontend || exit
 echo "Installing frontend dependencies and building the app..."
-npm install
-npm run build
+npm install && npm run build
 
 echo "Starting backend server..."
 cd ../backend || exit
 
 echo "Copy frontend build"
-cp -r ../frontend/dist ./public
 
 echo "Installing backend dependencies..."
-npm install
-npm run dev
+npm install && npm run build
+
+cp -r ../frontend/dist ./dist/public
+
+npm run prod
 
 echo "Production server is running. Access the app at http://localhost:3000"
